e2bf11ba077248e91cffd95d6c56b1e99bd981b2
Aspirin <samuellyworld@gmail.com>
2024-04-20 22:30:38 +0100

    fix : build

--- remitflex/frontend/src/services/protected/paymentAPI.ts
+++ remitflex/frontend/src/services/protected/paymentAPI.ts
@@ -1,6 +1,6 @@
 import * as paymentRoute from "../apiRoutes/paymentRoutes";
 import { protectedGet, protectedPost } from "../apiHelper";
-import { ICreateInvoice, IBillPayment } from "@/types/services.types";
+import { ICreateInvoice} from "@/types/services.types";
 
 export const paymentControl = {
   getNGNrate: async () => {
@@ -9,7 +9,4 @@ export const paymentControl = {
   createInvoice: async (remit: ICreateInvoice) => {
     return await protectedPost(paymentRoute.createInvoice, remit);
   },
-  billPayment: async (bill: IBillPayment) => {
-    return await protectedPost(paymentRoute.billPayment, bill);
-  }
 };
